3b1a8af5a0d4f4cb2b621a3957246f83fc402ff9
Anirudh Singh <singhab@singhab-w540.rose-hulman.edu>
2019-10-21 17:53:33 -0400

    changed readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,24 @@
 import React from "react"
-import {Link, graphql} from "gatsby";
-import Layout from '../components/layout'
-import BackgroundSection from "../components/global/backgroundsection";
-import Info from "../components/Home/info";
-import ProdMenu from "../components/Home/prodmenu"
-
-
-//background image is looking for data from graphql query- 
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import BackgroundSection from "../components/global/backgroundsection"
+import Info from "../components/Home/info"
+//background image is looking for data from graphql query-
 //fluid is passed in the BackgroundImage- so use that prop
-const IndexPage = ({data}) => (
-    <Layout>
-
-    <BackgroundSection img={data.img.childImageSharp.fluid} title="Bladesign!!" styleClass="default-background"/ >  
-    <Info/>  
-
-    </Layout>
-);
-
+const IndexPage = ({ data }) => (
+  <Layout>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Bladesign"
+      styleClass="default-background"
+    />
+    <Info />
+  </Layout>
+)
 
 export const query = graphql`
-{
-    img: file(relativePath: {eq: "bla-logo.png"}) {
+  {
+    img: file(relativePath: { eq: "bla-logo.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -28,25 +26,24 @@ export const query = graphql`
       }
     }
 
-
     prodmenu: allContentfulProductItem {
-    edges {
-      node {
-        id
-        title
-        description {
-          description
-        }
-        category
-        image{
-          fixed(width:50, height:50){
-            ...GatsbyContentfulFixed_tracedSVG
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              src
+            }
           }
         }
       }
     }
   }
-  }`;
+`
 
-
-export default IndexPage;
+export default IndexPage
--- src/pages/productDisplay.js
+++ src/pages/productDisplay.js
@@ -1,32 +1,33 @@
 import React from "react"
-import {Link, graphql} from "gatsby";
-import Layout from '../components/layout'
-import BackgroundSection from "../components/global/backgroundsection";
-import Info from "../components/Home/info";
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import BackgroundSection from "../components/global/backgroundsection"
+import Info from "../components/Home/info"
 import Products from "../components/Home/productS"
 
-
-//background image is looking for data from graphql query- 
+//background image is looking for data from graphql query-
 //fluid is passed in the BackgroundImage- so use that prop
-const ProductDisplay = ({data}) => (
-    <Layout>
-
-    <BackgroundSection img={data.img.childImageSharp.fluid} title="Product Line" styleClass="about-background"/ >  
-    <Products/>  
-    </Layout>
-);
-
+const ProductDisplay = ({ data }) => (
+  <Layout>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Product Line"
+      styleClass="about-background"
+    />
+    <Products />
+  </Layout>
+)
 
 export const query = graphql`
-{
-    img: file(relativePath: {eq: "candle-about.png"}) {
+  {
+    img: file(relativePath: { eq: "candle-about.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
-  }`;
+  }
+`
 
-
-export default ProductDisplay;
+export default ProductDisplay
